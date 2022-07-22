7b9fa46a182e04544eabf9d1d81da7f1112e5f4c
Gavin Davies <Gavin.Davies@derivco.co.uk>
2022-07-22 10:26:27 +0100

    Change to scroll in to view

--- html/RaceResults.php
+++ html/RaceResults.php
@@ -42,7 +42,7 @@ div.race-insights-chart {
 
 			$('#jaffa-race-results').empty();
 			getRace(raceId, false);
-			$('#jaffa-race-results').scrollTop(0);
+			document.getElementById("jaffa-race-results").scrollIntoView();
 		});
 
 		// 1. Get race
