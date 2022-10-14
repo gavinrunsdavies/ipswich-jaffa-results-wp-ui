0b51351fb7bed65bc596a3b707170bb3aa6732f7
Gavin Davies <Gavin.Davies@derivco.co.uk>
2022-10-14 13:36:35 +0100

    Add latest results as a new menu item

--- Program.php
+++ Program.php
@@ -7,94 +7,107 @@ Version: 3.0
 Author: Gavin Davies
 Author URI: https://github.com/gavinrunsdavies/
 */
+
 namespace IpswichJAFFARunningClubResults;
-		
-require_once plugin_dir_path( __FILE__ ) .'widgets/LatestResultsWidget.php';
-require_once plugin_dir_path( __FILE__ ) .'widgets/RunnerOfTheMonthWidget.php';
-		
+
+require_once plugin_dir_path(__FILE__) . 'widgets/LatestResultsWidget.php';
+require_once plugin_dir_path(__FILE__) . 'widgets/RunnerOfTheMonthWidget.php';
+//require_once plugin_dir_path(__FILE__) . 'php/LatestResultsMenuItems.php';
+
+use IpswichJAFFARunningClubAPI\Php\LatestResultsMenuItems as LatestResultsMenuItems;
+
 $go = new Program();
 
-class Program 
+class Program
 {
-	const JQUERY_HANDLE= 'jquery';	
-
+	const JQUERY_HANDLE = 'jquery';
 	const JQUERY_DATATABLES_HANDLE = 'jquery.dataTables.min';
 	const JQUERY_DATATABLES_RESPONSIVE_HANDLE = 'dataTables.responsive.min';
 
-	function __construct() {		
+	public function __construct()
+	{
 		add_action('init', array($this, 'registerShortCodes'));
 		add_action('wp_enqueue_scripts', array($this, 'styles'));
 		add_action('widgets_init', array($this, 'registerWidgets'));
+		$latestResultsMenu = new LatestResultsMenuItems();
+		add_filter('wp_get_nav_menu_items', array($latestResultsMenu, 'addLatestResultsMenuItems'), 10, 2);
 	}
-		
-	public function registerShortCodes()	{				
-		add_shortcode('ipswich-jaffa-running-club-results', array( $this, 'processShortCode' ));
-	}	
-		
+
+	public function registerShortCodes()
+	{
+		add_shortcode('ipswich-jaffa-running-club-results', array($this, 'processShortCode'));
+	}
+
 	public function styles()
-	{	
+	{
 		wp_enqueue_style(
-		    'IpswichJaffaResults.css',
-			plugins_url('/lib/IpswichJaffaResults.css', __FILE__ )
+			'IpswichJaffaResults.css',
+			plugins_url('/lib/IpswichJaffaResults.css', __FILE__)
 		);
 
 		wp_enqueue_style(
-		    'jquery.dataTables.min.css',
+			'jquery.dataTables.min.css',
 			'https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css'
 		);
 
 		wp_enqueue_style(
-		    'responsive.dataTables.min.css',
+			'responsive.dataTables.min.css',
 			'https://cdn.datatables.net/responsive/2.2.9/css/responsive.dataTables.min.css'
-		);		
-	}
-		
-	public function registerWidgets() {
-		register_widget( 'IpswichJAFFARunningClubResults\LatestResultsWidget' );
-		register_widget( 'IpswichJAFFARunningClubResults\RunnerOfTheMonthWidget' );
+		);
 	}
 
-	public function processShortCode($attr, $content = "") {
+	public function registerWidgets()
+	{
+		register_widget('IpswichJAFFARunningClubResults\LatestResultsWidget');
+		register_widget('IpswichJAFFARunningClubResults\RunnerOfTheMonthWidget');
+	}
+
+	public function processShortCode($attr, $content = "")
+	{
 		$atts = shortcode_atts(
-			array('memberresultspageid' => 0, 
-				  'eventresultspageid' => 0,
-				  'feature' => ''), 
-			$attr);
-		
+			array(
+				'memberresultspageid' => 0,
+				'eventresultspageid' => 0,
+				'feature' => ''
+			),
+			$attr
+		);
+
 		// These are used in the feature files.
-		$memberResultsPageUrl = get_permalink($atts['memberresultspageid']);	
-		$eventResultsPageUrl = get_permalink($atts['eventresultspageid']);		
+		$memberResultsPageUrl = get_permalink($atts['memberresultspageid']);
+		$eventResultsPageUrl = get_permalink($atts['eventresultspageid']);
 		$raceResultsPageUrl = get_permalink($atts['eventresultspageid']);
-		
-		$feature = $atts['feature'];		
-		
-		if ($feature != '') {			
+
+		$feature = $atts['feature'];
+
+		if ($feature != '') {
 			// Load scripts in the footer and only for the shortcode. Styles are loaded for all and in the header
 			$this->scripts();
-			
+
 			ob_start();
 			$filename = "html/$feature.php";
-			if (file_exists(dirname(__FILE__).'/'.$filename)) {
+			if (file_exists(dirname(__FILE__) . '/' . $filename)) {
 				require_once $filename;
 			} else {
 				echo "Filename $filename does not exist.";
 			}
 			$content = ob_get_clean();
 		}
-		
+
 		return $content;
 	}
-		
-	private function scripts() {
+
+	private function scripts()
+	{
 		wp_enqueue_script(self::JQUERY_HANDLE);
-			
+
 		wp_enqueue_script(
 			self::JQUERY_DATATABLES_HANDLE,
 			'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js',
 			array(self::JQUERY_HANDLE),
 			null,
 			true
-		);				
+		);
 
 		wp_enqueue_script(
 			self::JQUERY_DATATABLES_RESPONSIVE_HANDLE,
@@ -106,11 +119,10 @@ class Program
 
 		wp_enqueue_script(
 			'IpswichJaffaResults.js',
-			plugins_url('/lib/IpswichJaffaResults.js?version=1.1.1', __FILE__ ),
+			plugins_url('/lib/IpswichJaffaResults.js?version=1.1.1', __FILE__),
 			null,
 			null,
 			true
 		);
 	}
 }
-?>
