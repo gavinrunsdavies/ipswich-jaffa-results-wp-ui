0b2132f168cf3ddc05869096dfd3322f888ad7d9
Gavin Davies <gavinrunsdavies@gmail.com>
2023-08-08 16:42:24 +0100

    Remove generated data

---
+++ html/RunnerResultsCategoryChart.php
@@ -0,0 +1,130 @@
+<script src="https://cdn.amcharts.com/lib/5/index.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/hierarchy.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
+<script>
+
+jQuery(document).ready(function($) {
+
+  $.ajax('<?php echo esc_url( home_url() ); ?>/wp-json/ipswich-jaffa-api/v2/statistics/resultCountByCategoryAndCourse')
+    .done(function(data) {
+      createRunnerResultsCountChart(data);
+    });	
+
+  function createRunnerResultsCountChart(data) {
+
+    am5.ready(function() {
+
+      var root = am5.Root.new("chartdiv");
+
+      const myTheme = am5.Theme.new(root);
+
+      myTheme.rule("RoundedRectangle", ["hierarchy", "node", "shape", "depth1"]).setAll({
+        strokeWidth: 2
+      });
+
+      myTheme.rule("RoundedRectangle", ["hierarchy", "node", "shape", "depth2"]).setAll({
+        fillOpacity: 0,
+        strokeWidth: 1,
+        strokeOpacity: 0.2
+      });
+
+      myTheme.rule("Label", ["node", "depth1"]).setAll({
+        forceHidden: true
+      });
+
+      myTheme.rule("Label", ["node", "depth2"]).setAll({
+        fontSize: 10
+      });
+
+      root.setThemes([
+        am5themes_Animated.new(root),
+        myTheme
+      ]);
+
+      // Create wrapper container
+      var container = root.container.children.push(
+        am5.Container.new(root, {
+          width: am5.percent(100),
+          height: am5.percent(100),
+          layout: root.verticalLayout
+        })
+      );
+
+      // Create series
+      // https://www.amcharts.com/docs/v5/charts/hierarchy/#Adding
+      var series = container.children.push(
+        am5hierarchy.Treemap.new(root, {
+          sort: "descending",
+          singleBranchOnly: false,
+          downDepth: 1,
+          upDepth: 0,
+          initialDepth: 1,
+          valueField: "count",
+          categoryField: "name",
+          childDataField: "courseTypes",
+          nodePaddingOuter: 0,
+          nodePaddingInner: 0
+        })
+      );
+
+      series.get("colors").set("step", 1);
+
+
+      container.children.moveValue(
+        am5hierarchy.BreadcrumbBar.new(root, {
+          series: series
+        }), 0
+      );
+
+      // {
+      // "categoryName":"LU20",
+      // "courseTypes":[
+      //    {
+      //       "name":"Undefined",
+      //       "count":"28"
+      //    },
+      //    {
+      //       "name":"Cross Country",
+      //       "count":"47"
+      //    },
+
+function processData(data) {
+  var treeData = [];
+
+  return [{
+    name: "Home",
+    courseTypes: data
+  }];
+}
+
+series.data.setAll(processData(data));
+series.set("selectedDataItem", series.dataItems[0]);
+
+series.bullets.push(function (root, series, dataItem) {
+  var depth = dataItem.get("depth");
+
+  if (depth == 1) {
+    var picture = am5.Picture.new(root, {
+      src: "https://www.amcharts.com/wp-content/uploads/assets/logos/" + dataItem.dataContext.name.toLowerCase() + ".png",
+      centerX: am5.p50,
+      centerY: am5.p50,
+      width: am5.percent(30),
+      isMeasured: true
+    });
+
+    picture.states.lookup("default").setAll({ opacity: 0.15 });
+
+    return am5.Bullet.new(root, { sprite: picture });
+  }
+});
+
+}); // end am5.ready()
+  } // end function createRunnerResultsCountChart()
+});
+</script>
+<script>
+
+</script>
+<div> 
+	<div id="chartdiv" style="width: 100%; height: 700px; max-width:100%; clear: both;"></div>
+</div>
