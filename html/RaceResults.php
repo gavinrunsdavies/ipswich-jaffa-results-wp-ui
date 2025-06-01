e390a2571dd2c385464892b8547f94aafd9ed663
Gavin Davies <gavinrunsdavies@gmail.com>
2025-06-01 20:12:50 +0100

    Upgrade Datatable library and remove custom icons for responsive

--- html/RaceResults.php
+++ html/RaceResults.php
@@ -49,21 +49,6 @@ a.jaffa-name {
 .jaffa-orange {
 	color: #e88112;
 }
-/* Datatables.net responsive design. Change expand and collapse arrows */
-td.dtr-control::before {
-    font-family: 'Material Symbols Outlined';
-    content: 'keyboard_arrow_down';
-    font-size: 24px;
-    vertical-align: middle;
-    cursor: pointer;
-    margin-right: 8px;
-    color: #555;
-}
-
-/* When row is expanded, show up arrow */
-tr.dtr-hasChild td.dtr-control::before {
-    content: 'keyboard_arrow_up';
-}
 </style>
 <div class="section">
 	<h2 id="jaffa-event-title"></h2>
@@ -85,7 +70,7 @@ tr.dtr-hasChild td.dtr-control::before {
 <script type="text/javascript">
     const link = document.createElement('link');
     link.rel = 'stylesheet';
-    link.href = 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=calendar_month,check_small,groups,keyboard_arrow_down,keyboard_arrow_up,landscape_2,laps,run_circle,sports,sprint,star,travel_explore,workspace_premium';
+    link.href = 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=calendar_month,check,groups,landscape_2,laps,run_circle,sports,sprint,star,travel_explore,workspace_premium';
     document.head.appendChild(link);
 	
 	jQuery(document).ready(function ($) {
@@ -397,7 +382,7 @@ tr.dtr-hasChild td.dtr-control::before {
 									}
 								}
 
-								return '<span class="material-symbols-outlined md-18">check_small</span>' + improvementHtml;
+								return '<span class="material-symbols-outlined md-18">check</span>' + improvementHtml;
 							}
 							return '';
 						},
