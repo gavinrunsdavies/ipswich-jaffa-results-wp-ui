ad20fba5f5fc58d3f4f8d41ee7c1fd510240d290
Gavin Davies <gavinrunsdavies@gmail.com>
2025-07-16 19:36:40 +0100

    WIT runner detail as row details

--- html/GrandPrix.php
+++ html/GrandPrix.php
@@ -15,168 +15,84 @@
 		</form>
 	</div>
 	<div id="men-grand-prix-results" style="display:none">		
-		<table class="display" id="men-grand-prix-races">
-			<caption>Men's Grand Prix Races</caption>	
-			<thead>
-				<tr>
-					<th>Event</th>
-					<th>Date</th>
-					<th>Description</th>
-					<th>Course Type</th>
-					<th>Conditions</th>
-					<th>Venue</th>
-					<th>Distance</th>
-				</tr>
-			</thead>
-			<tbody>
-			</tbody>
-		</table>
 		<table class="display grandprix-table" id="men-grand-prix-results-table" style="width:100%">	
 			<caption>Men's Grand Prix Current Standings</caption>				
 			<thead>
 				<tr>
+					<th></th>
 					<th>Name</th>
 					<th>Category</th>
 					<th>Total Points</th>
 					<th>Best 8 Points</th>		
-					<th>Details</th>
 				</tr>
 			</thead>
 			<tbody>
 			</tbody>					
 		</table>		
 	</div>
-	<div id="ladies-grand-prix-results" style="display:none" class="center-panel">		
-		<div class="row">
-			<div class="col-md-12">
-				<table class="display" id="ladies-grand-prix-races">
-					<caption style="font-weight:bold; padding: 0.5em">Ladies Grand Prix Races</caption>	
-					<thead>
-						<tr>
-							<th>Event</th>
-							<th>Date</th>
-							<th>Description</th>
-							<th>Course Type</th>
-							<th>Conditions</th>
-							<th>Venue</th>
-							<th>Distance</th>
-						</tr>
-					</thead>
-					<tbody>
-					</tbody>
-				</table>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-11">
-				<table class="display grandprix-table" id="ladies-grand-prix-results-table" style="width:100%">	
-					<caption style="font-weight:bold; padding: 0.5em">ladies Grand Prix Current Standings</caption>				
-					<thead>
-						<tr>
-							<th>Name</th>
-							<th>Category</th>
-							<th>Total Points</th>
-							<th>Best 8 Points</th>		
-							<th>Details</th>
-						</tr>
-					</thead>
-					<tbody>
-					</tbody>				
-				</table>
-			</div>	
-		</div>			
+	<div id="ladies-grand-prix-results" style="display:none" class="center-panel">
+		<table class="display grandprix-table" id="ladies-grand-prix-results-table" style="width:100%">	
+			<caption>Ladies Grand Prix Current Standings</caption>				
+			<thead>
+				<tr>
+					<th></th>
+					<th>Name</th>
+					<th>Category</th>
+					<th>Total Points</th>
+					<th>Best 8 Points</th>
+				</tr>
+			</thead>
+			<tbody>
+			</tbody>				
+		</table>		
 	</div>	
 </div>
 <script type="text/javascript">
-	jQuery(document).ready(function($) {					
-		$(function () {
-		  $('[data-toggle="popover"]').popover();
-		});	
+	jQuery(document).ready(function($) {				
 		 
 		function createDataTable(tableId, data, sexId) {
-			var tableBody = $('#' + tableId + ' tbody');
-			tableBody.empty();
 				
-			var rows = '';
-			$.each(data, function(i, runner){
-				rows += '<tr id = "' + runner.id + '" data-sex-id="' + sexId + '">';
-				var resultsUrl = '<?php echo $memberResultsPageUrl; ?>';
-				var anchor = '<a href="' + resultsUrl;						
-				anchor += '?runner_id=' + runner.id;						
-				anchor += '">' + runner.name + '</a>';		
-				rows += '<td>' + anchor + '</td>';
-				rows += '<td>' + runner.categoryCode + '</td>';
-				rows += '<td>' + runner.totalPoints + '</td>';
-				rows += '<td>' + runner.best8Score+ '</td>';
-				rows += '<td class="text-center"><a tabindex="0" role="button"><i class="fa fa-ellipsis-h grand-prix-detail" aria-hidden="true"></i></a></td>';				
-				rows += '</tr>';
-			});				
-	
-			var table = $('#' + tableId);
-			if (table.DataTable() != null) {
-				table.DataTable().clear();
-				table.DataTable().destroy();				
-			}
-				
-			
-			tableBody.append(rows);
-			
-			table.DataTable( {
-				pageLength : 10,
-				paging : true,
-				destory	   : true,	
-				searching: 	true,
-				order: 		[[ 3, "desc" ], [ 2, "desc" ]]
-			} );
-		}
-		
-		function populateRacesTable(tableId, data) {
-			var tableBody = $('#' + tableId + ' tbody');
-			tableBody.empty();
-				
-			var rows = '';
-			$.each(data, function(i, race){
-				rows += '<tr>';				
-				var eventResultsUrl = '<?php echo $eventResultsPageUrl; ?>';
-				var anchor = '<a href="' + eventResultsUrl;
-				if (eventResultsUrl.indexOf("?") >= 0) {
-					anchor += '&raceId=' + race.id;
-				} else {
-					anchor += '?raceId=' + race.id;
-				}
-				anchor += '">' + race.eventName + '</a>';	
-				rows += '<td>'+ anchor + '</td>';
-				rows += '<td>' + race.date + '</td>';
-				rows += '<td>' + nullToEmptyString(race.description)+ '</td>';
-				rows += '<td>' + nullToEmptyString(race.courseType) + '</td>';				
-				rows += '<td>' + nullToEmptyString(race.conditions) + '</td>';
-				rows += '<td>' + nullToEmptyString(race.venue) + '</td>';
-				rows += '<td>' + nullToEmptyString(race.distance) + '</td>';							
-				rows += '</tr>';
-			});
-			
-			tableBody.append(rows);
+			$(tableId).DataTable({
+				paging: true,
+				destroy : true,
+				searching: true,
+				processing: true,
+				autoWidth: false,
+				scrollX: false,
+				pageLength: 10,
+				order: [[ 3, "desc" ], [ 2, "desc" ]],
+				columns: [
+					{
+						className: 'dt-control',
+						orderable: false,
+						data: null,
+						defaultContent: ''
+					},
+					{
+						data: "results.name",
+						searchable: true,
+						sortable: true,
+						render: function(data, type, row, meta) {
+							var resultsUrl = '<?php echo $memberResultsPageUrl; ?>';
+							var anchor = '<a href="' + resultsUrl;
+							anchor += '?runner_id=' + row.id;
+							anchor += '">' + data + '</a>';
 
-			$('#' + tableId).DataTable( {
-				paging : false,
-				searching: false,
-				processing: false
-			} );
-		}
-		
-		function nullToEmptyString(value) {
-			return (value == null) ? "" : value;
-		}
-		
-		function getAjaxRequest(url) {
-			return {
-				"url" : '<?php echo esc_url( home_url() ); ?>' + url,
-				"method" : "GET",
-				"headers" : {
-					"cache-control" : "no-cache"
-				},
-				"dataSrc" : ""
-			};
+							return anchor;
+						}
+					},
+					{
+						data: "results.categoryCode"
+					},
+					{
+						data: "results.totalPoints"
+					},
+					{
+						data: "results.best8Score"
+					}
+				],
+				ajax: data
+			});
 		}
 		
 		var mensGPData;
@@ -191,10 +107,8 @@
 			  '<?php echo esc_url( home_url() ); ?>/wp-json/ipswich-jaffa-api/v2/results/grandprix/' + year + '/' + 2,
 			  function(data) {		
 				mensGPData = data.results;
-				
 				mensOrderedRacesIds = data.races;
-				populateRacesTable('men-grand-prix-races', data.races);
-				createDataTable('men-grand-prix-results-table', data.results, 2);
+				createDataTable('#men-grand-prix-results-table', data, 2);
 				
 				$('#men-grand-prix-results').show();
 			  }
@@ -204,17 +118,15 @@
 			  '<?php echo esc_url( home_url() ); ?>/wp-json/ipswich-jaffa-api/v2/results/grandprix/' + year + '/' + 3,
 			  function(data) {		
 				ladiesGPData = data.results;
-				
 				ladiesOrderedRacesIds = data.races;
-				populateRacesTable('ladies-grand-prix-races', data.races);
-				createDataTable('ladies-grand-prix-results-table', data.results, 3);
+				createDataTable('#ladies-grand-prix-results-table', data, 3);
 				
 				$('#ladies-grand-prix-results').show();
 			  }
 			);				
 		});
 		
-		function getResultDetailHtml(runner, orderedRacesIds) {
+		function getRunnerResultDetails(data) {
 						
 			var table = '<table class="display"><thead><tr><th>Race</th><th>Points</th></tr></thead><tbody>';
 					
@@ -245,48 +157,51 @@
 			
 			return table;
 		}
+
+		$('.grandprix-table').on('click', 'td.dt-control', function (e) {
+			let tr = e.target.closest('tr');
+			let row = table.row(tr);
 		
-		$('.grandprix-table tbody td a .grand-prix-detail').click(function () {
-			var nTr = this.parentNode.parentNode.parentNode;
-			var sexId = $(nTr).data('sex-id');			
+			if (row.child.isShown()) {
+				// This row is already open - close it
+				row.child.hide();
+			}
+			else {
+				// Open this row
+				row.child(getRunnerResultDetails(row.data())).show();
+			}
+		});
+		
+		// $('.grandprix-table tbody td a .grand-prix-detail').click(function () {
+		// 	var nTr = this.parentNode.parentNode.parentNode;
+		// 	var sexId = $(nTr).data('sex-id');			
 				
-			var runnerId = nTr.id;
-			var data, orderedRacesIds;
+		// 	var runnerId = nTr.id;
+		// 	var data, orderedRacesIds;
 		
-			if (sexId == 2) {
-				data = mensGPData;
-				orderedRacesIds = mensOrderedRacesIds;				
-			} else {
-				data = ladiesGPData;
-				orderedRacesIds = ladiesOrderedRacesIds;
-			}
+		// 	if (sexId == 2) {
+		// 		data = mensGPData;
+		// 		orderedRacesIds = mensOrderedRacesIds;				
+		// 	} else {
+		// 		data = ladiesGPData;
+		// 		orderedRacesIds = ladiesOrderedRacesIds;
+		// 	}
 		
-			var runner = findById(data, runnerId);
-			if (runner == null)
-				return;
+		// 	var runner = findById(data, runnerId);
+		// 	if (runner == null)
+		// 		return;
 		
-			var html = getResultDetailHtml(runner, orderedRacesIds);			
-		
-			// Hide any exitsing
-			$('.grandprix-table tbody td .grand-prix-detail').popover('destroy');
+		// 	var html = getResultDetailHtml(runner, orderedRacesIds);			
 			
-			var popover = $(this).popover({
-				title : runner.name,
-				content : html,
-				placement : 'right',
-				container: 'body',
-				trigger: 'click',
-				html: 'true'					
-			}).popover('show');		
-		} );
+		// } );
 		
-		function findById(source, id) {
-		  for (var i = 0; i < source.length; i++) {
-			if (source[i].id === id) {
-			  return source[i];
-			}
-		  }
-		  return null;
-		}		
+		// function findById(source, id) {
+		//   for (var i = 0; i < source.length; i++) {
+		// 	if (source[i].id === id) {
+		// 	  return source[i];
+		// 	}
+		//   }
+		//   return null;
+		// }		
 	});
 </script>
