4e165209becfc184bfd1a0bbb1d912e0a9440e93
Gavin Davies <gavinrunsdavies@gmail.com>
2026-07-20 16:55:07 +0100

    Remoev unwanted function call

--- html/RunnerProfile.php
+++ html/RunnerProfile.php
@@ -363,7 +363,7 @@
                                 if (distanceId == distanceId2) {
                                     rows += '<td class="success"><strong>' + formatPerformance(data[year][distanceId].performance, distanceId) + '</strong></td>';
                                 } else {
-                                    rows += '<td>' + formatPerformance(getPredictedTime(distanceId, data[year][distanceId].performance, distanceId2), distanceId2) + '</td>';
+                                    rows += '<td>' + getPredictedTime(distanceId, data[year][distanceId].performance, distanceId2) + '</td>';
                                 }
                             } else {
                                 rows += '<td></td>';
@@ -409,7 +409,7 @@
                             if (distanceId == distanceId2) {
                                 rows += '<td class="success"><strong>' + formatPerformance(data[distanceId].performance, distanceId) + '</strong></td>';
                             } else {
-                                rows += '<td>' + formatPerformance(getPredictedTime(distanceId, data[distanceId].performance, distanceId2), distanceId2) + '</td>';
+                                rows += '<td>' + getPredictedTime(distanceId, data[distanceId].performance, distanceId2) + '</td>';
                             }
                         } else {
                             rows += '<td></td>';
